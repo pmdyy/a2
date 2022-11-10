dadf06213bd00b447a1d18673042d74fccc86df5
pmdyy <39233499+pmdyy@users.noreply.github.com>
2022-11-09 23:01:14 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,9 +11,11 @@
 ;@ Below, edit to list any people who helped you with the code in this file,
 ;@      or put ‘none’ if nobody helped (the two of) you.
 ;@
-;@ Helpers: _everybody helped us/me with the assignment (list names or put ‘none’)__
+;@ Helpers: _everybody helped us/me with the assignment (list names or put
+‘none’)__
 ;@
-;@ Also, reference resources beyond the course textbook and the course pages on Canvas
+;@ Also, reference resources beyond the course textbook and the course pages on
+Canvas
 ;@ that you used in making your submission.
 ;@
 ;@ Resources:  ___________
@@ -29,8 +31,8 @@
 ******************************************************************************/
 // fibonacci program using recursion
 
-#include <iostream>
 #include <chrono>
+#include <iostream>
 // *** add more include files if you would like ***
 #include "bignumber.h"
 
@@ -38,55 +40,41 @@
 using namespace std;
 using namespace std::chrono;
 
-using fibonacciType1 = long;  // create an alias declaration (a type alias)
-//using fibonacciType1 = BigNumber;  // create an alias declaration (a type alias)
-//using fibonacciType2 = BigNumber;  // create an alias declaration (a type alias)
-
-// Recursive function fibonacci
-// This function is a slightly modified version of the function provided
-// by Deitel & Associates, Inc.
-fibonacciType1 fibonacci(long number) {
-   if ((0 == number) || (1 == number)) { // base cases
-      return number;
-   }
-   else { // recursion step
-      return fibonacci(number - 1) + fibonacci(number - 2);
-   }
-}
+using fibonacciType1 = BigNumber; // create an alias declaration (a type alias)
 
 int main() {
 
-   cout << "Do not change this line.  Enter a sequence of increasing Fibonacci indicies and -1 to stop input." << endl;
+  cout << "Do not change this line.  Enter a sequence of increasing Fibonacci "
+          "indicies and -1 to stop input."
+       << endl;
 
-   // Section 1
-   // *** insert here a loop to input Fibonacci indexes and calculate Fibonacci numbers, or input -1 to stop input ***
-   int n{-2};
-   int index{0};
-   while (
-         cin >> n,
-         -1 != n) {
-      index = n;
-      cout << index << endl;
-      cout << fibonacci(index) << endl;
-      //cout << fibonacci<fibonacciType1>(index) << endl;
-   }
+  // Section 1
+  // *** insert here a loop to input Fibonacci indexes and calculate Fibonacci
+  // numbers, or input -1 to stop input ***
+  int n{-2};
+  int index{0};
+  while (cin >> n, -1 != n) {
+    index = n;
+    cout << index << endl;
+    // cout << fibonacci(index) << endl;
+    cout << fibonacci<fibonacciType1>(index) << endl;
+  }
 
-   // Section 2
-   // use the next line as is to capture the start time of a 2 second period
-   time_point<steady_clock> start = steady_clock::now();
+  // Section 2
+  // use the next line as is to capture the start time of a 2 second period
+  time_point<steady_clock> start = steady_clock::now();
 
-   const int runLimit{2}; // generate Fibonacci numbers for 2 more seconds
+  const int runLimit{2}; // generate Fibonacci numbers for 2 more seconds
 
-   while((steady_clock::now() - start)/1s < runLimit) { // true if 2 seconds since start time have not yet elapsed
-      ; // *** put code here to generate more Fibonacci numbers. ***
-      fibonacci(++index);
-//      fibonacci<fibonacciType2>(++index);
-   }
+  while ((steady_clock::now() - start) / 1s <
+         runLimit) { // true if 2 seconds since start time have not yet elapsed
+    ; // *** put code here to generate more Fibonacci numbers. ***
+    fibonacci<fibonacciType1>(++index);
+    //      fibonacci<fibonacciType2>(++index);
+  }
 
-   // *** output the highest Fibonacci index calculated ***
-   cout << index << endl;
+  // *** output the highest Fibonacci index calculated ***
+  cout << index << endl;
 
-   return 0;
+  return 0;
 }
-
-
